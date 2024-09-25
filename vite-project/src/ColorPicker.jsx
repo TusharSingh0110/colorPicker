ea9460ebe2cd32f192967652fd3b2f034427b6e2
Tushar-Singh1 <100280218+TusharSingh0110@users.noreply.github.com>
2024-09-25 14:18:41 +0530

    Initial commit for Color Picker project

---
+++ vite-project/src/ColorPicker.jsx
@@ -0,0 +1,21 @@
+import react,{useState} from 'react';
+
+function ColorPicker(){
+    const[color,setColor]=useState("#ffffff");
+
+    function handleColorChange(e){
+        setColor(e.target.value);
+    }
+
+    return(
+        <div className='container'>
+            <h1>Color Picker</h1>
+            <div className='color-display' style={{backgroundColor: color}}>
+                <p>Selected Color:{color}</p>
+            </div>
+            <label>Select a Color:</label>
+            <input type="color" value={color} onChange={handleColorChange}></input>
+        </div>
+    );
+}
+export default ColorPicker;
